c90e96f96989d8ba6ad7e305b130887e47690404
Ronaldo Dominguez <matheus.dominguez@hotmail.com>
2021-04-21 16:38:57 -0300

    Primeiro commit

---
+++ projeto_nlw/src/controllers/SettingsController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { SettingsService } from "../services/SettingsService";
+
+class SettingsController {
+  async create(req: Request, res: Response) {
+    const { chat, username } = req.body;
+    const settingsService = new SettingsService();
+    try {
+      const settings = await settingsService.create({ chat, username });
+      res.json(settings);
+    } catch (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+  }
+}
+
+export { SettingsController };
